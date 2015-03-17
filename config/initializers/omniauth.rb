c3b4f7f24f7620aeba14b967ac5d1e34817887a7
Thiago Ribeiro <thiagitosouza@hotmail.com>
2015-03-16 21:49:12 -0300

    Fix app_id is riquired closes #18
    Signed-off-by: Thiago Ribeiro <thiagitosouza@hotmail.com>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['200578776773676'], ENV['72c5e68c4e10f2725a1a7a2b37542c74']
+  provider :facebook, '200578776773676', '72c5e68c4e10f2725a1a7a2b37542c74'
 end
